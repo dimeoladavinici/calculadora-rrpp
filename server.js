8310f5357d5437008da5ea1d13a2962de9631dc6
Juan Pedro Di meola <juan.dimeola@davinci.edu.ar>
2025-03-27 18:34:40 -0300

    actualizacion ruta index

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname)));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 app.post('/calcular', (req, res) => {
   const { lotes, combisLlenas } = req.body;
